5a6818b48fedcca53391f80b005904568ba3e0c0
sophie-shao <sophie_sha0@brown.edu>
2025-03-09 15:05:48 -0400

    navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,15 +16,15 @@ export default function RootLayout({
         <header className="header">
           <nav className="max-w-6xl mx-auto px-4 py-3 flex justify-between items-center" style={{ fontFamily: 'Quicksand, sans-serif' }}>
             <ul className="flex font-bold" style={{ flex: 1 }}>
-              <li className="mr-10"><a href="/" className="text-black hover:underline">Home</a></li>
-              <li className="mr-10"><a href="/mentors" className="text-black hover:underline">Mentors</a></li>
-              <li className="mr-10"><a href="/newsletter" className="text-black hover:underline">News</a></li>
+              <li className="mr-12"><a href="/" className="text-black hover:underline">Home</a></li>
+              <li className="mr-12"><a href="/mentors" className="text-black hover:underline">Mentors</a></li>
+              <li className="mr-12"><a href="/newsletter" className="text-black hover:underline">News</a></li>
             </ul>
             <Image src="/logo.png" alt="logo" width={100} height={85} layout="fixed"/>
             <ul className="flex font-bold" style={{ flex: 1 }}>
-              <li className="ml-10"><a href="/curriculum" className="text-black hover:underline">Curriculum</a></li>
-              <li className="ml-10"><a href="/gallery" className="text-black hover:underline">Gallery</a></li>
-              <li className="ml-10"><a href="/about" className="text-black hover:underline">About</a></li>
+              <li className="ml-12"><a href="/curriculum" className="text-black hover:underline">Curriculum</a></li>
+              <li className="ml-12"><a href="/gallery" className="text-black hover:underline">Gallery</a></li>
+              <li className="ml-12"><a href="/about" className="text-black hover:underline">About</a></li>
             </ul>
           </nav>
         </header>
