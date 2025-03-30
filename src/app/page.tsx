bda6c110eb320b241171dc4ddd8f2d529ebf1980
Serena Agarwal <serenaagarwal@DESKTOP-NJQE78Q.localdomain>
2025-03-30 12:51:51 -0400

    who we are

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,28 +9,46 @@ const quicksand = Quicksand({
 
 export default function Home() {
   return (
-    <div className="home-container">
-      <Image
-        src="/homeImage.png"
-        alt="Environmental Illustration"
-        width={490}
-        height={433}
-        className="home-image"
-      />
-      <div className={`text-content ${quicksand.className}`}>
-        <h1 className={"title"}>
-          Outdoor
-          <br />
-          Leadership and
-          <br />
-          <span className="outlineEnv">Environmental</span>
-          <br />
-          Education Program
-        </h1>
-        <button className="donate-button">Donate</button>
+    <div className ="main-container">
+      <div className="home-container">
+        <Image
+          src="/homeImage.png"
+          alt="Environmental Illustration"
+          width={490}
+          height={433}
+          className="home-image"
+        />
+        <div className={`text-content ${quicksand.className}`}>
+          <h1 className={"title"}>
+            Outdoor
+            <br />
+            Leadership and
+            <br />
+            <span className="outlineEnv">Environmental</span>
+            <br />
+            Education Program
+          </h1>
+          <button className="donate-button">Donate</button>
+        </div>
       </div>
 
+        <div className="who-we-are-container">
+          <h1 className={`who-we-are ${quicksand.className}`}>Who We Are</h1>
+          <p className={`who-we-are-text ${quicksand.className}`}> OLEEP is a partnership between the Metropolitan Regional Career and Technical Center (the Met) and Brown University’s Swearer Center for Public Service. We work with high school students to develop leadership skills as well as environmental science and justice knowledge, with a specific emphasis on building a supportive, collaborative community where <strong>Brown</strong> and <strong>Met</strong> students can learn and grow together. 
+            <br />
+            <br />
+            We believe, with the right support, everyone can be a leader and bringing about an environmentally just world depends on it.
+          </p>
+        </div>
+    
+
       <style>{`
+
+        .main-container {
+          display: flex;
+          align-items: center;
+          flex-direction: column;
+        }
         .home-container {
           background: linear-gradient(135deg, #DFF2D8, #83D28D);
           width: 100%;
@@ -78,6 +96,24 @@ export default function Home() {
         .donate-button:hover {
           background-color: #FFC107;
         }
+
+        .who-we-are-container {
+          text-align: center;
+          padding: 50px 20px;
+          width: 100%;
+        }
+
+        .who-we-are {
+          font-size: 36px;
+          font-weight: bold;
+        }
+
+        .who-we-are-text {
+          font-size: 24px;
+          padding: 50px 150px;
+        }
+
+        
 
       `}</style>
     </div>
